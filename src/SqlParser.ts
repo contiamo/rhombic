89476b218fb547a9e055ff4442535dfaceb913c9
Fabien BERNARD <fabien0102@hotmail.com>
2020-04-11 08:43:53 +0200

    Add tableAlias.*

--- src/SqlParser.ts
+++ src/SqlParser.ts
@@ -635,22 +635,22 @@ class SqlParser extends CstParser {
     this.OR([
       {
         ALT: () => {
+          this.OPTION1(() => {
+            this.CONSUME1(Identifier);
+            this.CONSUME(Period);
+          });
+          this.CONSUME(Asterisk);
+        }
+      },
+      {
+        ALT: () => {
           this.SUBRULE(this.expression);
           this.OPTION(() => {
             this.CONSUME(As);
             this.CONSUME(Identifier);
           });
         }
-      },
-      { ALT: () => this.CONSUME(Asterisk) }
-      // Need to learn how to backtrack to resolve ambiguous alternatives with the first rule
-      // {
-      //   ALT: () => {
-      //     this.CONSUME1(Identifier);
-      //     this.CONSUME(Period);
-      //     this.CONSUME1(Asterisk);
-      //   }
-      // }
+      }
     ]);
   });
 
