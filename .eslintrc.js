64003b1add27802195201579521e4724a93ed963
Michael Franzkowiak <michael@contiamo.com>
2021-09-06 12:41:21 +0200

    Add exception for unused params prefixed with _ to eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
-    "@typescript-eslint/explicit-module-boundary-types": "off"
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_", ignoreRestSiblings: true }
+    ]
   },
   overrides: [
     {
