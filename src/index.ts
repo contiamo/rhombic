a5e13ffe5df7b6f4a0effe99a114d2d22e07c54c
Michael Franzkowiak <michael@contiamo.com>
2021-09-21 10:57:57 +0200

    Export LineageHelper.

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ import { GroupByVisitor } from "./visitors/GroupByVisitor";
 
 // Antlr parser version
 export { default as antlr } from "./antlr";
+export { LineageHelper } from "./antlr/LineageHelper";
 
 // Utils
 export { needToBeEscaped, printFilter };
